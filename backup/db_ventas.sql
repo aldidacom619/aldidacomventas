73dc79ce0ec71aa85b4927abdb7ab11a37fa9389
Diego DazaAlcaraz <diegodaza.da619@gmail.com>
2020-08-21 15:53:03 -0400

    HOLAA

--- backup/db_ventas.sql
+++ backup/db_ventas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-08-2020 a las 01:38:36
+-- Tiempo de generación: 21-08-2020 a las 21:51:43
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.1
 
